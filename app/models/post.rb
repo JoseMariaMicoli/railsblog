6c18f5ad4e32eb56283e7dfc5a86aba973ac276e
Jose Maria Micoli <josemaria.micoli@gmail.com>
2015-03-26 19:32:23 -0300

    add very basic comments to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
 	validates_presence_of [:title,:body]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326033359) do
+ActiveRecord::Schema.define(version: 20150326205145) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.text     "body",       limit: 65535
+    t.integer  "post_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -20,4 +30,5 @@ ActiveRecord::Schema.define(version: 20150326033359) do
     t.datetime "updated_at",               null: false
   end
 
+  add_foreign_key "comments", "posts"
 end

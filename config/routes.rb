3edf370f95f342ce78e9faa32d03741ba373416b
Jose Maria Micoli <josemaria.micoli@gmail.com>
2015-03-26 19:56:22 -0300

    about as add

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+  get '/about', to: 'pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

57a31e079699188c327d62b6e0e4b9ac704ffbf1
Jose Maria Micoli <josemaria.micoli@gmail.com>
2015-04-04 00:37:01 -0300

    finaly fixed the bugs in contact and about static page with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!, except: ['index', 'show']
+  before_action :authenticate_user!, except: ['index', 'show', 'about']
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, :alert => exception.message
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,6 @@
 class ContactsController < ApplicationController
+  before_action :authenticate_user!, except: ['new']
+
 	def new
     	@contact = Contact.new
   	end
